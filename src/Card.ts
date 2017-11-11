b6ee96bab36b1ace9ba16319e025f702a8a93d57
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-11-12 00:50:05 +0900

    initial commit

---
+++ src/Card.ts
@@ -0,0 +1,51 @@
+import * as Im from "immutable";
+
+export interface CardData {
+  id?: string;
+  title?: string;
+  description?: string;
+  comments?: Im.List<Comment>;
+  due?: Date;
+}
+
+export class Comment {
+  id: string;
+  text: string;
+  createdAt: Date;
+  editedAt: Date;
+}
+
+// Im.Record の仕様上、defaultValues に項目がないと
+// コンストラクタで渡しても undefined になるので注意
+const defaultValues: CardData = {
+  id: null,
+  title: null,
+  description: null,
+  comments: null,
+  due: null,
+};
+
+export class Card extends Im.Record(defaultValues) {
+
+  static validate(values: CardData): CardData {
+    if (!values.title) {
+      throw new Error(`title は必須です`);
+    }
+    return values;
+  }
+
+  constructor(values?: CardData) {
+    super(Card.validate(values));
+  }
+
+  readonly id: string;
+  readonly title: string;
+  readonly description: string;
+  readonly comments: Im.List<Comment>;
+  readonly due: Date;
+
+  with(values: CardData) {
+    return this.merge(Card.validate(values)) as this;
+  }
+}
+
---
+++ src/__tests__/Card.ts
@@ -0,0 +1,90 @@
+import * as Im from "immutable";
+import {Card, CardData, Comment} from "../Card";
+
+describe("Card", () => {
+  describe("constructor", () => {
+    test("正しくエンティティが生成されること", () => {
+      const data: CardData = {
+        id: null,
+        title: "カードのタイトルです",
+        description: "カードの詳細説明です",
+        comments: Im.List([new Comment()]),
+        due: new Date(2017, 11, 12),
+      };
+      const entity = new Card(data);
+
+      // 各フィールドに過不足なく反映されること
+      expect(entity.id).toBeNull();
+      expect(entity.title).toBe("カードのタイトルです");
+      expect(entity.description).toBe("カードの詳細説明です");
+      expect(entity.comments.size).toBe(1);
+      expect(entity.comments).toEqual(Im.List([new Comment()]));
+      expect(entity.due).toEqual(new Date(2017, 11, 12));
+    });
+    test("不正な値ではエンティティが生成されないこと", () => {
+      const data: CardData = {
+        id: null,
+        title: null,
+        description: "カードの詳細説明です",
+        comments: Im.List([new Comment()]),
+        due: new Date(2017, 11, 12),
+      };
+
+      expect(() => new Card(data)).toThrow();
+    });
+  });
+  describe("with", () => {
+    test("エンティティはイミュータブルであること", () => {
+      const data: CardData = {
+        id: null,
+        title: "カードのタイトルです",
+        description: "カードの詳細説明です",
+        comments: Im.List([new Comment()]),
+        due: new Date(2017, 11, 12),
+      };
+      const entity = new Card(data);
+
+      const updated: CardData = {
+        title: "カードのタイトルを変更しました",
+        due: new Date(2017, 11, 13),
+      };
+
+      const newEntity = entity.with(updated);
+
+      // entity はイミュータブル
+      expect(entity.id).toBeNull();
+      expect(entity.title).toBe("カードのタイトルです");
+      expect(entity.description).toBe("カードの詳細説明です");
+      expect(entity.comments.size).toBe(1);
+      expect(entity.comments).toEqual(Im.List([new Comment()]));
+      expect(entity.due).toEqual(new Date(2017, 11, 12));
+
+      // newEntity には反映されている
+      expect(newEntity.id).toBeNull();
+      expect(newEntity.title).toBe("カードのタイトルを変更しました");
+      expect(newEntity.description).toBe("カードの詳細説明です");
+      expect(newEntity.comments.size).toBe(1);
+      expect(newEntity.comments).toEqual(Im.List([new Comment()]));
+      expect(newEntity.due).toEqual(new Date(2017, 11, 13));
+    });
+    test("不正な値では変更エンティティが生成されないこと", () => {
+      const data: CardData = {
+        id: null,
+        title: "カードのタイトルです",
+        description: "カードの詳細説明です",
+        comments: Im.List([new Comment()]),
+        due: new Date(2017, 11, 12),
+      };
+      const entity = new Card(data);
+
+      const updated: CardData = {
+        title: null,
+        due: new Date(2017, 11, 13),
+      };
+
+      // 不正なデータでは変更できない
+      expect(() => entity.with(updated)).toThrow();
+    });
+  });
+});
+
